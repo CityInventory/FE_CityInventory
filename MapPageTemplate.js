25bef147bf67cab5dbfb0002ce6c74f3c246fa5d
VBota1 <viorel.bota@agilenetworks.tech>
2021-08-14 02:41:07 +0300

    code cleanup administrare

--- MapPageTemplate.js
+++ MapPageTemplate.js
@@ -28,9 +28,24 @@ export function loadMap(mapToLoad) {
   }).addTo(mapToLoad);
 }
 
+export function getPinOptionsPopup(pinName) {
+  let popup = L.DomUtil.create('LI', 'options');
+  popup.style.listStyle = "none";
+
+  let title = L.DomUtil.create('h5');
+  title.innerHTML = pinName;
+  popup.appendChild(title);
+
+  return popup;
+}
+
+export function setMapButtonStyle(element) {
+  element.setAttribute("class", "btn btn-info btn-fill btn-wd options-btn");
+  element.style.color = 'white';
+}
+
 // ISSUES TABLE
-export function initIssueTableFilters(customStyles = () => {
-}) {
+export function initIssueTableFilters(customStyles = () => {}) {
   getAllIssuesView().then(issueList => showStyledIssues(issueList, customStyles));
 
   document.getElementById('toate').addEventListener('click', function () {
@@ -42,14 +57,17 @@ export function initIssueTableFilters(customStyles = () => {
     getIssuesViewByPinType(1).then(issueList => showStyledIssues(issueList, customStyles));
     setActiveFilterButton('cladiri');
   });
+
   document.getElementById('drumuri').addEventListener('click', function () {
     getIssuesViewByPinType(2).then(issueList => showStyledIssues(issueList, customStyles));
     setActiveFilterButton('drumuri');
   });
+
   document.getElementById('spatiiDeschise').addEventListener('click', function () {
     getIssuesViewByPinType(3).then(issueList => showStyledIssues(issueList, customStyles));
     setActiveFilterButton('spatiiDeschise');
   });
+
   document.getElementById('altele').addEventListener('click', function () {
     getIssuesViewByPinType(4).then(issueList => showStyledIssues(issueList, customStyles));
     setActiveFilterButton('altele');
@@ -91,57 +109,58 @@ async function showIssues(issuesViewArray) {
   issuesViewArray.forEach((issueView) => {
     let row = document.createElement("tr");
 
-    let issueIdCell = document.createElement("td");
-    issueIdCell.innerHTML = issueView.issueId;
-    row.append(issueIdCell)
-
-    let pinNameCell = document.createElement("td");
-    pinNameCell.innerHTML = issueView.pinName;
-    row.append(pinNameCell)
-
-    let descriptionCell = document.createElement("td");
-    descriptionCell.innerHTML = issueView.description;
-    row.append(descriptionCell)
-
     let statusCell = document.createElement("td");
-    let statusSelector = document.createElement("select")
-    statusSelector.classList.add("issue-status-form-selector");
-    statusSelector.classList.add("btn");
-    statusSelector.classList.add("btn-primary");
-    statusSelector.classList.add("btn-default");
-    statusSelector.classList.add("btn-primary-customization");
-    loadIssueStatusOptions(statusSelector, statusList, issueView.statusId);
-    statusSelector.addEventListener('change', (event) => {
-      updateIssueStatus(event.target.value, issueView);
-    });
-    statusCell.append(statusSelector);
-    row.append(statusCell)
-
-    let dateCell = document.createElement("td");
-    dateCell.innerHTML = issueView.date.toDateString();
-    row.append(dateCell)
+    statusCell.append(getStatusSelect(statusList, issueView.statusId));
 
     let optionsCell = document.createElement("td");
-    optionsCell.classList.add("btn-group");
-    optionsCell.classList.add("table-options");
+    optionsCell.append(getRemoveIssueButton(issueView.issueId, row));
 
-    let buttonClasses = "btn btn-sm btn-round btn-primary table-btn btn-primary-customization".split(" ");
-    let removeButton = document.createElement("button");
-    buttonClasses.forEach(cssClass => removeButton.classList.add(cssClass));
-    removeButton.innerHTML = "Șterge";
-    removeButton.addEventListener('click', () => {
-      removeIssue(issueView.issueId, (operationSucceeded) => {
-        if (operationSucceeded) {
-          row.remove();
-        }
-      })
-    });
-    optionsCell.append(removeButton);
-
+    row.append(getTableCellWithContent(issueView.issueId));
+    row.append(getTableCellWithContent(issueView.pinName));
+    row.append(getTableCellWithContent(issueView.description));
+    row.append(statusCell)
+    row.append(getTableCellWithContent(issueView.date.toDateString()))
     row.append(optionsCell)
 
     issuesTableBody.append(row);
   });
+}
+
+function getTableCellWithContent(content) {
+  let newCell = document.createElement("td");
+  newCell.innerHTML = content;
+  return newCell;
+}
+
+function getStatusSelect(statusList, selectedStatusId) {
+  let statusSelector = document.createElement("select")
+
+  statusSelector.setAttribute("class","issue-status-form-selector btn btn-primary btn-default btn-primary-customization");
+
+  loadIssueStatusOptions(statusSelector, statusList, selectedStatusId);
+
+  statusSelector.addEventListener('change', (event) => {
+    updateIssueStatus(event.target.value, issueView);
+  });
+
+  return statusSelector;
+}
+
+function getRemoveIssueButton(issueId, tableRow) {
+  let removeButton = document.createElement("button");
+
+  removeButton.setAttribute("class", "btn btn-sm btn-round btn-primary table-btn btn-primary-customization");
+  removeButton.innerHTML = "Șterge";
+
+  removeButton.addEventListener('click', () => {
+    removeIssue(issueId, (operationSucceeded) => {
+      if (operationSucceeded) {
+        tableRow.remove();
+      }
+    })
+  });
+
+  return removeButton;
 }
 
 function updateIssueStatus(selectValue, issueView) {
@@ -153,12 +172,12 @@ function updateIssueStatus(selectValue, issueView) {
 function removeIssue(issueId, callback) {
   deleteIssue(issueId)
     .then(_ => {
-      alert(`Sesizarea cu numărul ${issueId} a fost ștearsă.`);
+      alert(`Sesizarea cu numărul "${issueId}" a fost ștearsă.`);
       callback(true);
     })
     .catch(error => {
       console.log('error', error)
-      alert(`Sesizarea cu numărul ${issueId} nu a fost ștearsă. Te rugăm să încerci din nou.`);
+      alert(`Sesizarea cu numărul "${issueId}" nu a fost ștearsă. Te rugăm să încerci din nou.`);
       callback(false);
     });
 }
@@ -166,7 +185,7 @@ function removeIssue(issueId, callback) {
 function updateIssue(message) {
   putIssue(message)
     .then(result => {
-      alert(`Sesizarea cu numărul ${result.data.id} a fost modificată.`);
+      alert(`Sesizarea cu numărul "${result.data.id}" a fost modificată.`);
     })
     .catch(error => {
       console.log('error', error)
@@ -181,10 +200,31 @@ export async function getIssueStatusValues() {
 
 export function loadIssueStatusOptions(parentSelectElement, statusList, selectedStatusId = 1) {
   statusList.forEach(status => {
-    var option = document.createElement("option");
+    let option = document.createElement("option");
     option.text = arrayToSeparatedString(status.id, status.name);
     if (selectedStatusId == status.id)
       option.selected = true;
     parentSelectElement.add(option);
   });
+}
+
+//GENERIC FORM
+export function addInputFieldValidations() {
+  let targetedElements = document.getElementsByClassName("requires-validation");
+  for (let element of targetedElements) {
+    element.addEventListener('change', (ev) => setValidityStyle(ev.target));
+  }
+}
+
+export function hasInvalidValue(element) {
+  return element.value.trim() === '';
+}
+
+export function setValidityStyle(element) {
+  if (hasInvalidValue(element)) {
+    element.style.border = '2px solid red';
+  }
+  else {
+    element.style.border = '1px solid black';
+  }
 }
